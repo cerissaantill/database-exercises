37aac72b0330558b7c3522baf850e417c5d1a8a8
Cerissa Antill <cerissa.antill@gmail.com>
2019-04-11 11:10:52 -0500

    completed Functions exercise

---
+++ functions_exercises.sql
@@ -0,0 +1,53 @@
+USE EMPLOYEES;
+
+
+
+-- 1. find all employees with first names Irena, Vidya, Maya
+-- 2. update query to use OR instead of IN (did not do)
+-- 3. add a condition to find everyone with those names who is also male
+-- 4. order results by first name (ascending)
+-- 5. order results by last name (ascending)
+
+SELECT *
+FROM employees
+WHERE first_name IN ('IRENA', 'VIDYA', 'MAYA')
+AND gender = 'M'
+ORDER BY first_name ASC, last_name ASC;
+
+
+
+-- 1. find all employees whose last name starts with E
+-- 2. find all employees whose last name starts or ends with E
+-- 3. sort results by employee number (descending)
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%'
+OR last_name LIKE '%E'
+ORDER BY emp_no DESC;
+
+
+-- 1. duplicate above query and update it to find all employees
+--      whose last name starts AND ends with E
+-- 2. use concat() to combine their first and last name together as a single
+--      column in your result.
+
+SELECT employee_no, birth_date, concat(first_name, ' ', last_name) AS 'full_name', gender, hire_date
+FROM employees
+WHERE last_name LIKE 'E%'
+AND last_name LIKE '%E'
+ORDER BY emp_no DESC;
+
+
+-- 1. find all employees hired in the 90s, and born on Christmas
+-- 2. order results by the oldest employee who was hired last
+-- 3. use datediff() to find how many days they have been working at the company
+--      [hint: you will use now() and curdate() ]
+SELECT first_name, last_name, hire_date, birth_date, datediff(curdate(), hire_date)
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+AND birth_date LIKE '%-12-25'
+ORDER BY birth_date ASC, hire_date DESC;
+
+
+
