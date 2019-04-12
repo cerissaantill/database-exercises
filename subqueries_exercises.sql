5ea831f05c812ff3a263997cf924cb60f3d6ad0c
Cerissa Antill <cerissa.antill@gmail.com>
2019-04-12 12:13:41 -0500

    exercise complete, bonus incomplete

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -1,21 +1,39 @@
 USE EMPLOYEES;
 
+
 -- 1. Find all employees with the same hire date as employee 101010 using a sub-query (69 rows)
-
-
-
-
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date IN (
+SELECT hire_date
+FROM employees
+WHERE emp_no = 101010
+);
 
 
 -- 2. Find all titles held by all employees with the first name Aamod (314 total, 6 unique titles)
-
-
-
-
+SELECT title
+FROM titles
+WHERE emp_no IN
+  (SELECT emp_no
+  FROM employees
+  WHERE first_name = 'Aamod')
+;
 
 
 -- 3. Find all the current dept managers that are female (4)
-
+SELECT first_name, last_name
+FROM employees
+WHERE emp_no IN
+  (SELECT emp_no
+  FROM dept_manager
+  WHERE emp_no IN
+       (SELECT emp_no
+        FROM employees
+        WHERE gender = 'F'
+        )
+  AND to_date > now()
+  )
 
 
 
