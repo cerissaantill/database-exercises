351a176df06c8309eddae9c3c78fa249f0821a87
Cerissa Antill <cerissa.antill@gmail.com>
2019-04-12 11:47:06 -0500

    Sub-Queries exercise questions

---
+++ subqueries_exercises.sql
@@ -0,0 +1,47 @@
+USE EMPLOYEES;
+
+-- 1. Find all employees with the same hire date as employee 101010 using a sub-query (69 rows)
+
+
+
+
+
+
+-- 2. Find all titles held by all employees with the first name Aamod (314 total, 6 unique titles)
+
+
+
+
+
+
+-- 3. Find all the current dept managers that are female (4)
+
+
+
+
+
+
+
+-- BONUS
+-- 1. Find all the dept names that currently have female managers
+
+
+
+
+
+-- 2. Find the first and last name of the employees with the highest salary
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
